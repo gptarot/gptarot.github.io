80da862a0874671d99a94e72926b6b8197bc9e31
greyyT <nhannguyen8338@gmail.com>
2023-07-26 21:55:21 +0700

    Add tailwind to front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p className='text-red-600'>Hello World</p>
     </>
   )
 }
