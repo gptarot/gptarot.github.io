65c360ff12b7e9adff0c3441db10fe2b35b0eed0
thangbuiq <21280048@student.hcmus.edu.vn>
2023-07-26 21:31:24 +0700

    Backend init

---
+++ backend/run.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    @app.route('/api', methods=['GET'])
+    def api():
+        return jsonify({'result': 'You will be loved!'})
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
