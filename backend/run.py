be3eea18c9488e6090ecb912139f9f7e536f76b0
thangbuiq <21280048@student.hcmus.edu.vn>
2023-07-27 12:55:57 +0700

    Testing vercel

--- backend/run.py
+++ backend/run.py
@@ -5,11 +5,12 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    @app.route('/api', methods=['GET'])
+    @app.route('/', methods=['GET'])
     def api():
         return jsonify({'result': 'You will be loved!'})
 
     return app
+
 
 if __name__ == '__main__':
     app = create_app()
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,5 +1,4 @@
-from run import create_app
+from run import app
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
