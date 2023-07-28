8fb9c310c5c58618400a0f8a7045ea34f70048d4
thangbuiq <21280048@student.hcmus.edu.vn>
2023-07-28 23:04:18 +0700

    Backend - Update commands.sh

--- backend/commands.sh
+++ backend/commands.sh
@@ -3,4 +3,4 @@
 # This script is used to export the environment variables from the .env file
 export $(xargs < .env)
 
-gunicorn --bind 0.0.0.0:7496 --workers 5 --timeout 120 api.wsgi:app
+gunicorn --bind "0.0.0.0:$PORT" --workers 5 --timeout 120 api.wsgi:app
