aa5dd60b6d18e43ac9cd3e869e26b2fb6010e509
greyyT <nhannguyen8338@gmail.com>
2023-07-29 21:49:37 +0700

    Fix router

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import getPrediction from '../services/tarotService';
+import getPrediction from '../api/tarotAPI';
 import Input from '../components/Input';
 import { Cards, Prediction } from '../types';
 import { Link } from 'react-router-dom';
@@ -15,7 +15,7 @@ const Home: React.FC = (): JSX.Element => {
   });
   const [prediction, setPrediction] = useState<Prediction | null>(null);
 
-  const handleSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (ev: React.FormEvent<HTMLFormElement>): Promise<void> => {
     ev.preventDefault();
 
     const response: Prediction | undefined = await getPrediction(
@@ -33,52 +33,59 @@ const Home: React.FC = (): JSX.Element => {
   };
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-background bg-center">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <Input
-          id="name"
-          label="Your name"
-          onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setName(ev.target.value)}
-          value={name}
-        />
-        <Input
-          id="dob"
-          label="Your Day of Birth"
-          onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setDob(ev.target.value)}
-          value={dob}
-        />
-        <Input
-          id="question"
-          label="Your Question"
-          onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setQuestion(ev.target.value)}
-          value={question}
-        />
-        <div className="flex gap-4">
+    <div className="bg-[#21212c] h-screen flex flex-col items-center justify-center relative">
+      <div className="absolute inset-0 bg-background bg-center opacity-20" />
+      <div className="flex flex-col items-center justify-center z-10">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <Input
-            id="pastCard"
-            label="Past Card"
-            onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setCards({ ...cards, pastCard: ev.target.value })}
-            value={cards.pastCard}
+            id="name"
+            label="Your name"
+            onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setName(ev.target.value)}
+            value={name}
           />
           <Input
-            id="presentCard"
-            label="Present Card"
-            onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setCards({ ...cards, presentCard: ev.target.value })}
-            value={cards.presentCard}
+            id="dob"
+            label="Your Day of Birth"
+            onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setDob(ev.target.value)}
+            value={dob}
           />
           <Input
-            id="futureCard"
-            label="Future Card"
-            onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setCards({ ...cards, futureCard: ev.target.value })}
-            value={cards.futureCard}
+            id="question"
+            label="Your Question"
+            onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setQuestion(ev.target.value)}
+            value={question}
           />
-        </div>
-        <div className="flex items-center justify-center">
-          <button className="bg-white px-4 py-2 rounded-md">Submit</button>
-        </div>
-      </form>
-      {prediction && <div className="bg-white p-4 rounded-md mt-10">{prediction.data}</div>}
-      <Link to="/random">Navigate</Link>
+          <div className="flex gap-4">
+            <Input
+              id="pastCard"
+              label="Past Card"
+              onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setCards({ ...cards, pastCard: ev.target.value })}
+              value={cards.pastCard}
+            />
+            <Input
+              id="presentCard"
+              label="Present Card"
+              onChange={(ev: React.ChangeEvent<HTMLInputElement>) =>
+                setCards({ ...cards, presentCard: ev.target.value })
+              }
+              value={cards.presentCard}
+            />
+            <Input
+              id="futureCard"
+              label="Future Card"
+              onChange={(ev: React.ChangeEvent<HTMLInputElement>) =>
+                setCards({ ...cards, futureCard: ev.target.value })
+              }
+              value={cards.futureCard}
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <button className="bg-white px-4 py-2 rounded-md">Submit</button>
+          </div>
+        </form>
+        {prediction && <div className="bg-white p-4 rounded-md mt-10">{prediction.data}</div>}
+        <Link to="/random">Navigate</Link>
+      </div>
     </div>
   );
 };
--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Random from '../pages/Random';
 
 const Router: React.FC = (): JSX.Element => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="random" element={<Random />} />
         <Route path="/*" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
